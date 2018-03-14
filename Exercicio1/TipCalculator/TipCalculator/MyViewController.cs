d3e253d622107cc14654616e8c4908f1e8b005d0
Ricardo <riki1998200926@gmail.com>
2018-03-14 17:41:31 +0000

    innit

---
+++ Exercicio1/TipCalculator/TipCalculator/MyViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace TipCalculator
+{
+    public class MyViewController : UIViewController
+    {
+        public MyViewController()
+        {
+        }
+    }
+}
